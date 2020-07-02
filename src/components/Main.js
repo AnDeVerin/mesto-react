4d944501dd2cfda2ff61d9bafad52d9e0290d441
Alexander Zinchuk <alx.zinchuk@gmail.com>
2020-07-02 23:54:54 +0200

    User Info: Add state and effect to load from API

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import api from '../utils/api';
 
 function Main({ onEditProfile, onAddPlace, onEditAvatar }) {
+  const [userName, setUserName] = React.useState('');
+  const [userDescription, setUserDescription] = React.useState('');
+  const [userAvatar, setUserAvatar] = React.useState();
+
+  React.useEffect(() => {
+    api.getUserInfo().then((userData) => {
+      setUserName(userData.name);
+      setUserDescription(userData.about);
+      setUserAvatar(userData.avatar);
+    });
+  }, []);
+
+  const imageStyle = { backgroundImage: `url(${userAvatar})` };
+
   return (
     <main className="content">
       <section className="profile page__section">
-        <div className="profile__image" onClick={onEditAvatar}></div>
+        <div className="profile__image" onClick={onEditAvatar} style={imageStyle}></div>
         <div className="profile__info">
-          <h1 className="profile__title">Жак-Ив Кусто</h1>
+          <h1 className="profile__title">{userName}</h1>
           <button className="profile__edit-button" type="button" onClick={onEditProfile}></button>
-          <p className="profile__description">Исследователь океана</p>
+          <p className="profile__description">{userDescription}</p>
         </div>
         <button className="profile__add-button" type="button" onClick={onAddPlace}></button>
       </section>
