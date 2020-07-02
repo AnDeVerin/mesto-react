b69867bbdab995b5f0470b60a3569d08218a53ac
Alexander Zinchuk <alx.zinchuk@gmail.com>
2020-07-02 21:59:07 +0200

    Add click handlers to open popups (imperative)

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,28 @@
 import React from 'react';
 
 function Main() {
+  function handleEditAvatarClick() {
+    document.querySelector('.popup_type_edit-avatar').classList.add('popup_is-opened');
+  }
+
+  function handleEditProfileClick() {
+    document.querySelector('.popup_type_edit').classList.add('popup_is-opened');
+  }
+
+  function handleAddPlaceClick() {
+    document.querySelector('.popup_type_new-card').classList.add('popup_is-opened');
+  }
+
   return (
     <main className="content">
       <section className="profile page__section">
-        <div className="profile__image"></div>
+        <div className="profile__image" onClick={handleEditAvatarClick}></div>
         <div className="profile__info">
           <h1 className="profile__title">Жак-Ив Кусто</h1>
-          <button className="profile__edit-button" type="button"></button>
+          <button className="profile__edit-button" type="button" onClick={handleEditProfileClick}></button>
           <p className="profile__description">Исследователь океана</p>
         </div>
-        <button className="profile__add-button" type="button"></button>
+        <button className="profile__add-button" type="button" onClick={handleAddPlaceClick}></button>
       </section>
       <section className="places page__section">
         <ul className="places__list">
