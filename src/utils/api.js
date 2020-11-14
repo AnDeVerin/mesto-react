d4cb772f2775876df5a266373e0cb4c54cb2df19
h3yAlias <asthmaisback@gmail.com>
2020-11-15 00:48:14 +0300

    fix: change endpoint

--- src/utils/api.js
+++ src/utils/api.js
@@ -114,7 +114,7 @@ class Api {
 }
 
 const api = new Api({
-  address: 'https://praktikum.tk',
+  address: 'https://nomoreparties.co',
   groupId: `cohort0`,
   token: `80a75492-21c5-4330-a02f-308029e94b63`,
 });
